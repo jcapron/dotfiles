bf95844beac0c8c46c9539a72429ec1f55370e75
jcapron <elniafron62@gmail.com>
2014-01-31 20:39:09 -0500

    update

--- zsh_modules/aliases.sh
+++ zsh_modules/aliases.sh
@@ -1,8 +1,8 @@
 # .zshrc aliases
-alias zshrc="subl $HOME/.zshrc $HOME/Code/More/dotfiles"
+alias zshrc="s $HOME/.zshrc $HOME/Code/More/dotfiles"
 alias src="source $HOME/.zshrc"
-alias cl="clear"
 
 # Smartass aliases
 alias l='ls -lah'
 alias psaux='ps aux | grep'
+alias cl="clear"
