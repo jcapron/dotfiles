b6932b3c95323d33e799b53b9ade59c38efca07f
jcapron <elniafron62@gmail.com>
2014-02-12 15:16:41 -0500

    new git shortcut for heroku

--- zsh_modules/git.sh
+++ zsh_modules/git.sh
@@ -6,6 +6,7 @@ alias push='git push'
 
 # Eject button for git
 alias gfuck='git reset . && git checkout . && git clean -df'
+alias gphm='git push heroku master'
 
 # More aliases available in the git plugin for oh-my-zsh:
 # https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/git/git.plugin.zsh
